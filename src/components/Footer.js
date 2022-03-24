06139a8172763b91a98d9d575da6b1ec6914095b
asmit203 <asmit.ganguly@gmail.com>
2022-03-24 20:11:03 +0530

    putting on the skeleton of faq section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,15 +8,15 @@ const Footer = () => {
                     <div className="col-2">
                         <h5>Section</h5>
                         <ul className="nav flex-column">
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
+                            <li className="nav-item mb-2"><a href="/" className="nav-link p-0 text-muted">Home</a></li>
+                            {/* <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li> */}
+                            {/* <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li> */}
+                            <li className="nav-item mb-2"><a href="/faq" className="nav-link p-0 text-muted">FAQs</a></li>
+                            <li className="nav-item mb-2"><a href="/about" className="nav-link p-0 text-muted">About</a></li>
                         </ul>
                     </div>
 
-                    <div className="col-2">
+                    {/* <div className="col-2">
                         <h5>Section</h5>
                         <ul className="nav flex-column">
                             <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
@@ -36,7 +36,7 @@ const Footer = () => {
                             <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
                             <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
                         </ul>
-                    </div>
+                    </div> */}
 
                     <div className="col-4 offset-1">
                         <form>
